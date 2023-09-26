99f748cd79f73a842acef371e1d9953e938988d2
Anshu Sharma <anshusharma6327@gmail.com>
2023-09-26 17:07:25 +0530

    script updated & compression added

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import express from "express";
 import cors from "cors";
 import FileRouter from "./routes/File";
+import compression from "compression";
 import { ensureDirectoryExists } from "./constants/path";
 async function init() {
   const app = express();
   const PORT = Number(process.env.PORT) || 8000;
   app.use(express.json());
+  app.use(compression({
+    level:9
+  }));
   app.use(
     cors({
       origin: "*",
     })
   );
-   await ensureDirectoryExists();
+  await ensureDirectoryExists();
 
   app.use(FileRouter);
 
